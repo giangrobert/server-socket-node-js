d37da1e82c4dc883f5912c7a5a6ab1a1fabc5e05
Gian <gianmamani93@gmail.com>
2024-03-01 10:09:02 -0500

    seever sockert

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+
+//App Express
+const app = express();
+
+//Node Server
+const server = require('http').createServer(app);
+module.exports.io = require('socket.io')(server);
+require('./sockets/socket');
+//path pùblico 
+const publicPath = path.resolve(__dirname, 'public');
+app.use(express.static(publicPath));
+
+server.listen(process.env.PORT, (err) => {
+    if(err) throw new Error(err);
+    console.log('Server is running on port ', process.env.PORT);
+});
